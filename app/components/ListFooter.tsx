1fec77dded601ebec1c8ddb8a6b732b4a3b2225b
Davina Leong <leong.shi.yun@gmail.com>
2025-12-14 13:02:35 +0800

    Added the ListFooter component

---
+++ app/components/ListFooter.tsx
@@ -0,0 +1,28 @@
+import Pagination from "./Pagination"
+
+interface ListFooterProps {
+    // Pagination props
+    currentPage: number;
+    totalPages: number;
+    onPageChange: (page: number) => void;
+    
+    // General props
+    className?: string;
+}
+
+export default function ListFooter({
+    currentPage,
+    totalPages,
+    onPageChange,
+    className = ""
+}: ListFooterProps) {
+    return (
+        <footer className={className}>
+            <Pagination 
+                currentPage={currentPage}
+                totalPages={totalPages}
+                onPageChange={onPageChange}
+            />
+        </footer>
+    )
+}
--- app/faith/page.tsx
+++ app/faith/page.tsx
@@ -1,11 +1,13 @@
 import FaithHeroSection from "../components/sections/faith/FaithHeroSection"
 import ListHeader from "../components/ListHeader"
+import ListFooter from "../components/ListFooter"
 
 export default function Faith() {
     return (
         <div>
             <FaithHeroSection />
             <ListHeader />
+            <ListFooter />
         </div>
     )
 }
