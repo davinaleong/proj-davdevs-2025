15b981dc996173e80f92d1cf67c5c3ca545e0319
Davina Leong <leong.shi.yun@gmail.com>
2025-12-14 12:28:06 +0800

    Get links from the json file

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,15 @@ import HomeHeroSection from './components/sections/home/HomeHeroSection'
 import HomePostSection from './components/sections/home/HomePostSection'
 import HomeSocialSection from './components/sections/home/HomeSocialSection'
 import HomeJokeSection from './components/sections/home/HomeJokeSection'
+import { findLinkByLabel } from './utils/links'
 
 export default function Home() {
+  const faithLink = findLinkByLabel("Faith");
+  
   return (
     <div>
       <HomeHeroSection />
-      <HomePostSection title="Messages of Faith 🙏✨" postType="sermons" variant="primary" viewAllHref="/sermons" viewAllText="View All Sermons" />
+      <HomePostSection title="Messages of Faith 🙏✨" postType="sermons" variant="primary" viewAllHref={faithLink?.href || "/faith"} viewAllText="View All Sermons" />
       <HomeSocialSection />
       <HomeJokeSection />
     </div>
