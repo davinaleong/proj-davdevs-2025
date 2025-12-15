da107c91a77766cc2e6bb8691bb9042a2385b0b2
Davina Leong <leong.shi.yun@gmail.com>
2025-12-15 20:48:25 +0800

    Done with the inner page template.

--- app/faith/[slug]/page.tsx
+++ app/faith/[slug]/page.tsx
@@ -1,6 +1,11 @@
+'use client'
+
 import HeroSection from "./../../components/HeroSection"
 import LinkButton from "./../../components/LinkButton"
 import ImageDisplay from "./../../components/ImageDisplay"
+import Prose from "./../../components/Prose"
+import TagFlex from "../../components/TagFlex"
+import Tag from "./../../components/Tag"
 
 export default function FaithSlugPage() {
     return (
@@ -14,10 +19,26 @@ export default function FaithSlugPage() {
                     <LinkButton href="#">Link 2</LinkButton>
                 </p>
             </HeroSection>
-            <section className="container mx-auto md:p-4">
+            <section className="max-w-2xl mx-auto md:p-4">
                 <ImageDisplay alt="Placeholder image" aspectRatio="landscape"/>
                 {/* Post Type: Article(s) */}
                 <ImageDisplay alt="Placeholder image" aspectRatio="square"/>
+            </section>
+            <section className="container mx-auto p-4 flow">
+                <Prose>
+                    <h2>Section Heading</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+                    <h3>Subsection Heading</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+                </Prose>
+
+                <TagFlex>
+                    <Tag>Tag 1</Tag>
+                    <Tag>Tag 2</Tag>
+                    <Tag>Tag 3</Tag>
+                </TagFlex>
             </section>
         </div>
     )
