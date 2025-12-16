d1c9b8d683fdcb2aaa08d41a53a731b20164ab54
Davina Leong <leong.shi.yun@gmail.com>
2025-12-16 22:46:25 +0800

    Done with the Tools listing page.

--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -1,7 +1,20 @@
+import ToolsHeroSection from "../components/sections/tools/ToolsHeroSection"
+import PostsList from "../components/PostsList"
+import { getPostsByType } from "../utils/content"
+
 export default function Tools() {
+    // Get all sermon posts on server side
+    const allTools = getPostsByType('tools')
+
     return (
         <div>
-            Tools
+            <ToolsHeroSection />
+            <PostsList 
+                posts={allTools} 
+                postType="tools"
+                searchPlaceholder="Search tools..."
+                baseHref="/tools"
+            />
         </div>
     )
 }
