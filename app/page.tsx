c8a147d3491bef91c704b68112fcf375fe5be582
Davina Leong <leong.shi.yun@gmail.com>
2025-12-17 06:56:07 +0800

    Fixed home page link bug.

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,13 @@ import HomeJokeSection from './components/sections/home/HomeJokeSection'
 import { findLinkByLabel } from './utils/links'
 
 export default function Home() {
+  const toolsLink = findLinkByLabel("Tools");
   const faithLink = findLinkByLabel("Faith");
   
   return (
     <div>
       <HomeHeroSection />
-      <HomePostSection title="Tools & Utilities 🛠️" postType="tools" variant="neutral" viewAllHref={faithLink?.href || "/tools"} viewAllText="View All Tools" />
+      <HomePostSection title="Tools & Utilities 🛠️" postType="tools" variant="neutral" viewAllHref={toolsLink?.href || "/tools"} viewAllText="View All Tools" />
       <HomePostSection title="Messages of Faith 🙏✨" postType="sermons" variant="primary" viewAllHref={faithLink?.href || "/faith"} viewAllText="View All Sermons" />
       <HomeSocialSection />
       <HomeJokeSection />
