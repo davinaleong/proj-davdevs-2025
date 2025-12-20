e7c033d9f31af8ee93066c76aa0866efb0b4f7e5
Davina Leong <leong.shi.yun@gmail.com>
2025-12-20 16:19:36 +0800

    WIP knowledge sharing module.

--- app/components/sections/home/HomeKnowledgeSharingSection.tsx
+++ app/components/sections/home/HomeKnowledgeSharingSection.tsx
@@ -1,14 +1,15 @@
 import HomeSection from '../../HomeSection'
 import Card from '../../Card'
-import { getKnowledgeSharingPosts } from '../../../utils/site-config'
+import { getKnowledgeSharingPosts, getPostSectionContent } from '../../../utils/site-config'
 
 export default function HomeKnowledgeSharingSection() {
   const knowledgeSharingPosts = getKnowledgeSharingPosts();
+  const sectionContent = getPostSectionContent('knowledgeSharing');
 
   return (
     <HomeSection
-      title="Knowledge Sharing 📚"
-      variant="neutral"
+      title={sectionContent?.title || "Knowledge Sharing 📚"}
+      variant={sectionContent?.variant || "default"}
     >
       <div className="container mx-auto p-6">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
